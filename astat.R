3ddba40fadc952d8e23b54acbab4c2749a8c7eaf
projcompil <nissim.zerbib@ens.fr>
2014-05-23 16:56:10 +0200

    ...

--- astat.R
+++ astat.R
@@ -14,16 +14,22 @@ x = read.csv(args[6], comment.char = "#")
 #print(sd(x[,2]))
 X11()
 #plot(x[,2], x, ylab="errorrate", xlab="listsize")
-plot(x$n, x$errorate)
+#plot(x$n, x$errorate)
+plot(x[,2], x[,1], ylab="errorrate", xlab="listsize")
 temp <- data.frame(y = x[,1], x = x[,2])
 # fit non-linear model
-mod <- nls(y ~ tanh(a + b * x), data = temp, start = list(a = 0, b = 0))
+mod <- nls(y ~ 1/(1+exp(a + b *x)), data = temp, start = list(a = 0, b = 0))
 #
 coef = coef(mod)
 
+a = coef[1]
+b = coef[2]
 print(coef)
 # add fitted curve
-lines(temp$x, predict(mod, list(x = temp$x)))
+#lines(temp$x, predict(mod, list(x = temp$x)))
+func <-function(x) 1/(1+exp(a + b *x))
+curve(func, from = 0, to = 100, n = 100001, add=TRUE, col="red")
+#lines(temp$x, predict(mod, list(x = temp$x)))
 
 message("Press Return To Continue")
 invisible(readLines("stdin", n=1))
